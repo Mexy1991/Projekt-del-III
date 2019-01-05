87f3fbb66d7190332603adc4287385614ef25f93
Josephine <mexy1991@outlook.com>
2019-01-05 18:17:46 +0100

    compiles but Iterable is missing

--- EventQueue.java
+++ EventQueue.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
-public class EventQueue implements Iterable<Event>{
+//public class EventQueue implements Iterable<Event>{
+	
+/*
 	private int current = 0; 
 	private String[] contents;
 
@@ -26,7 +29,7 @@ public class EventQueue implements Iterable<Event>{
 		else return;
 		*/
 
-		Event nextEvent = queue.get(0);
+/*		Event nextEvent = queue.get(0);
 		ArrayList tempArrayList = queue;
 
 		for (ArrayList whatever : tempArrayList){
@@ -49,7 +52,7 @@ public class EventQueue implements Iterable<Event>{
 	*/
 			//return (current < contents.length);
 
-			if(queue =!isEmpty()) {
+		/*	if(queue =!isEmpty()) {
 			return true;
 			}
 			else{
@@ -59,6 +62,8 @@ public class EventQueue implements Iterable<Event>{
 
 			
 	}
+
+	*/
 
 
 /******
@@ -80,4 +85,79 @@ order in which they are returned by next().
 ******/
 
 
+public class EventQueue{
+	EventQueue queue;
+	ArrayList<Event> listOfEvents = new ArrayList<Event>();
+	//arryalist eller linkedlist fordi de er variable størrelser og ordered
+
+	public EventQueue(){
+		// Constructs a new empty queue
+		EventQueue queue = new EventQueue();
+		queue = this.queue;
+	}
+
+
+	public void add (Event e){
+		//Adds the event e to this event queue
+		queue.add(e);
+
+	}
+
+	public Event next(){
+		// Return the next event in the queue and removes it afterwards. 
+    	// Events are sorted after time
+    	// Precondition: There is a next event. (Ikke?)
+
+    	/*
+    	* OBS OBS OBS OBS - Jeg synes, det der forloop er liiidt meget 
+    	* copy-paste. Måske vi kan finde på en anden måde?
+    	* Måske det bliver bedre med Iterator
+    	*/
+		
+    	Event firstEvent = listOfEvents.get(0);
+    	ArrayList tempListOfEvents = listOfEvents;
+
+    	//Mangler hele tids-halløjet
+
+    	for (Object tempEvent : tempListOfEvents){
+    		if(((Event)tempEvent).time() < firstEvent.time())
+    			firstEvent = (Event)tempEvent;
+    	}
+
+		tempListOfEvents.remove(firstEvent);
+
+    	return firstEvent;
+
+    	
+
+    }
+
+    public boolean hasNext(){
+    	//This method returns true, if the event queue is not empty
+    	if(listOfEvents.isEmpty()==true)
+    		return false;
+    		else return true;
+    	}
 }
+
+    	
+
+
+
+
+
+
+
+		/*
+
+    Event firstEvent = listOfEvents.get(0);
+    Event nextEvent = listOfEvents.get(firstEvent+1);
+    ArrayList tempListOfEvents = listOfEvents;
+    for (nextEvent2 : tempListOfEvents) {
+      if (nextEvent.time() < firstEvent.time()) {
+        firstEvent = (Event)nextEvent;
+      }
+    }
+    tempListOfEvents.remove(firstEvent);
+    return firstEvent;
+    */
